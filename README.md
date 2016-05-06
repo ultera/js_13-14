50745f68628e7e5acd88069e3b29d3e3d3c1b05c
ultera <ann@tavrida.org>
2016-05-06 16:46:28 +0300

    1st commit

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 # js_13-14
+sourceTest.weight[i] - 0, if sourceTest.answers[i] is incorrect;
+                     - 1, if sourceTest.answers[i] is correct.
+
+Т.е., если только одна единичка (sourceTest.weight[1, 0, 0, 0, 0, 0]), то в вопросе только 1 правильный ответ,
+2 (sourceTest.weight[1, 1, 0, 0, 0, 0]) - 2 правильных ответа...
--- js/script.js
+++ js/script.js
@@ -1,45 +1,44 @@
 "use strict";
 $(function() {
 
+  /** sourceTest.weight[i] - 0, if sourceTest.answers[i] is incorrect;
+  /*                       - 1, if sourceTest.answers[i] is correct.*/
 
   var sourceTest = [
     {number: 0,
     question: "Which one of the five is least like the other four?",
-    answers: ["Dog", "Mouse", "Lion", "Snake", "Elephant", "I don't know"],
+    answers: ["Dog", "Mouse", "Lion", "Snake", "Elephant", "I'm not sure"],
     weight: [0, 0, 0, 1, 0, 1]},
 
     {number: 1,
     question: "Which number should come next in the series?",
-    answers: [8, 13, 21, 26, 31, "I don't know"],
+    answers: [8, 13, 21, 26, 31, "I'm not sure"],
     weight: [0, 0, 1, 0, 0, 1]},
 
     {number: 2,
     question: "Which one of the five choices makes the best comparison?",
-    answers: [25641, 26451, 12654, 51462, 15264, "I don't know"],
+    answers: [25641, 26451, 12654, 51462, 15264, "I'm not sure"],
     weight: [0, 0, 0, 0, 1, 1]},
 
     {number: 3,
     question: "Mary, who is sixteen years old, is four times as old as her brother. How old will Mary be when she is twice as old as her brother?",
-    answers: [20, 24, 25, 26, 28, "I don't know"],
+    answers: [20, 24, 25, 26, 28, "I'm not sure"],
     weight: [0, 1, 0, 0, 0, 1]},
 
     {number: 4,
     question: "Which one of the numbers does not belong in the following series?",
-    answers: ["THREE", "SEVEN", "EIGHT", "FIFTEEN", "THIRTY", "I don't know"],
+    answers: ["THREE", "SEVEN", "EIGHT", "FIFTEEN", "THIRTY", "I'm not sure"],
     weight: [0, 0, 1, 0, 0, 1]},
 
     {number: 5,
     question: "Which one of the five choices makes the best comparison? Finger is to Hand as Leaf is to:",
-    answers: ["Twig", "Tree", "Branch", "Blossom", "Bark", "I don't know"],
+    answers: ["Twig", "Tree", "Branch", "Blossom", "Bark", "I'm not sure"],
     weight: [1, 0, 0, 0, 0, 1]},
 
     {number: 6,
     question: "If you rearrange the letters 'CIFAIPC' you would have the name of a(n):",
-    answers: ["City", "Animal", "Ocean", "River", "Country", "I don't know"],
+    answers: ["City", "Animal", "Ocean", "River", "Country", "I'm not sure"],
     weight: [0, 0, 1, 0, 0, 1]}
-
-
-
   ];
 
   var $container = $(".container");
@@ -54,46 +53,53 @@ $(function() {
   $div.append($button);
 
   //........... CHECK RESULTS ...........
-  $button.on('click', function() {
+  $button.on('click', function(event) {
 
-    var $inputs = $("input[type='checkbox']:checked");
-    var number = [];
-    var userWeight = 0;
+    var userWeight = [];
+    var userResult = 0;
 
+    for(var i = 0; i < sourceTest.length; i++) {
 
-    for(var i = 0; i < $inputs.length; i++) {
-      if($inputs[i].checked) {
-        number = $inputs[i].className.split("-");
-        if(sourceTest[number[0]].weight[number[1]] == 1) {
-          userWeight++;
-          // $inputs[i].addClass(".right");
-        } else {
-          // $inputs[i].addClass(".wrong");
-        }
+      userWeight.length = 0;
+      for (var j = 0; j < sourceTest[i].answers.length; j++) {
+        userWeight[j] = +$("." + [i] + "-" + [j]).is(':checked');
+      }
+
+      if (userWeight.join() == sourceTest[i].weight.join()) {
+        userResult++;
+        $(".question" + i).addClass("right");
+      } else {
+        $(".question" + i).addClass("wrong");
       }
     };
-    console.log(userWeight);
-    console.log( calcWeight() );
 
-    $(".label:2-1").attr("background", "green");
-
-
-
-
+    $(".result").text(userResult + " from " + sourceTest.length);
+    showModal(event);
   });
 
-  function calcWeight() {
-    var weight = 0;
-    for(var i = 0; i < sourceTest.length; i++) {
-      for(var j = 0; j < sourceTest[i].weight.length; j++) {
-        weight += sourceTest[i].weight[j];
-      }
-    }
-    return weight;
+
+  function showModal(event) {
+    event.preventDefault();
+		$('.overlay').fadeIn(400,
+		  function() {
+				$('.modal')
+					.css('display', 'block')
+					.animate({opacity: 1, top: '45%'}, 200);
+        $('.modal-close').css('display', 'block');
+		});
   }
 
+  $('.modal-close, .overlay').click( function() {
+		$('.modal')
+			.animate( {
+        opacity: 0, top: '30%'}, 200,
+				function() {
+					$(this).css('display', 'none');
+					$('.overlay').fadeOut(400);
+		});
 
-
-
-
+    $("input[type='checkbox']").removeAttr('checked');
+    $('h4').removeClass("wrong");
+    $('h4').removeClass("right");
+	});
 })
